fa31f8e7ff00584775f548181081e1dc9c949794
Kabir Khan <kakh@microsoft.com>
2020-02-17 12:28:53 -0800

    fix test scripts

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,6 +3,6 @@
 set -e
 set -x
 
-mypy typer --disallow-untyped-defs
-black typer tests --check
-isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --recursive --check-only --thirdparty typer typer tests
+mypy nersights --disallow-untyped-defs
+black nersights tests --check
+isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --recursive --check-only --thirdparty nersights
